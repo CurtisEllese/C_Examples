4328214ed2de64b2d15868e11768995b6d2c4819
CurtisEllese <mama-karina@bk.ru>
2023-01-06 20:49:36 +0600

    Added Example002_HelloUser

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿// start => enter username => alert "Hello, " "<username>" => end
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
